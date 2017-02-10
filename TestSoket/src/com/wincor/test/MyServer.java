cbc1dc1c446fa47aaaf2acb0e3bf2492b4c4ac28
jiangxianghui007 <jiangxianghui007@163.com>
2017-02-10 16:16:07 +0800

    更新MyServer

--- TestSoket/src/com/wincor/test/MyServer.java
+++ TestSoket/src/com/wincor/test/MyServer.java
@@ -38,7 +38,7 @@ public class MyServer {
         	isr.close();
         	is.close();
         	socket.close();
-        	System.out.println("bbbb");
+        	System.out.println("ccccccc");
     	}
     	
     	  
